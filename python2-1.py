b869e6a626ded44fa115d21e7b0ccbaffd94693a
Hiroyuki Sasaki <spr1940502@stu.o-hara.ac.jp>
2021-07-22 11:22:39 +0900

    text2.3

---
+++ python2-1.py
@@ -0,0 +1,9 @@
+#x = 60
+if x >= 80:
+    print ( "A" )
+elif x >= 60:
+    print ( "B" )
+elif x >= 40:
+    print ( "C" )
+else:
+    print ( "D" )
---
+++ python2-2.py
@@ -0,0 +1,11 @@
+x = 60
+if x >= 80:
+    #mes = "excellent"
+elif x >= 60:
+    mes = "good"
+elif x >= 40:
+    mes = "passing"
+else:
+    mes = "failing"
+
+print ( mes )
