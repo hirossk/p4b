e018ab558100d3427701e8ad785ca9531fbdd5e7
Hiroyuki Sasaki <spr1940502@stu.o-hara.ac.jp>
2021-12-20 16:28:21 +0900

    filename

--- python8-1.py
+++
@@ -1,8 +0,0 @@
-def func(x):
-    answer = x ** 2 + 2 * x + 3
-    return answer
-#ここでインデント（字下げ）を戻すことで関数（まとまった手順）が終わったという意味ことになります。
-y = func(2) #ここで上に書いた関数funcを呼び出しています。
-print( y )
-y = func(4)
-print( y )
